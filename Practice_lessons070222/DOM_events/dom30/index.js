9137d5de62cdeef3bdb18464b813f8c38ed12435
Nguyen Duyen <s2200128@BC-C02GR0BVQ6LR-MacBook-Air.local>
2022-02-18 07:44:42 +0200

    fixed dom 30

--- Practice_lessons070222/DOM_events/dom30/index.js
+++ Practice_lessons070222/DOM_events/dom30/index.js
@@ -7,21 +7,20 @@ let tabbedContent = () => {
   for (let i = 0; i < tabs.length; i++) {
     // Add click event to all tabs
     tabs[i].addEventListener("click", function (e) {
-      for (let i = 0; i < tabs.length; i++) {
-        // remove 'active' from all tabs
-        tabs[i].classList.remove("active");
-      }
+      // remove 'active' from all tabs
+      tabs.forEach((tab) => tab.classList.remove("active"));
 
-      for (let i = 0; i < tabContents.length; i++) {
-        // remove 'show' class from all tab contents
-        tabContents[i].classList.remove("show");
-      }
+      // remove 'show' class from all tab contents
+      tabContents.forEach((tabContent) => tabContent.classList.remove("show"));
 
       // Get data-content attribute of what is clicked.
-      let matchingTab = e.currentTarget.getAttribute("data-content");
-      console.log(matchingTab);
-      document.querySelector(`${matchingTab}`).classList.add("show");
       e.currentTarget.classList.add("active");
+      const { content } = e.target.dataset;
+      document.querySelector(`${content}`).classList.add("show");
+      console.log("target:", e.target, "currentTarget:", e.currentTarget);
+      // let matchingTab = e.currentTarget.getAttribute("data-content");
+      // console.log(matchingTab);
+      // document.querySelector(`${matchingTab}`).classList.add("show");
     });
   }
 };
